04b43c0a3c276a8a300ee1117af4bb96bfd7a713
Mag <monstamagp@gmx.de>
2021-03-22 10:25:54 +0100

    hire extracted and loader added

--- src/mp_leitstellenspiel.js
+++ src/mp_leitstellenspiel.js
@@ -5,38 +5,11 @@ var mp_speed=["Realistisch", "Normal", "Schnell", "Turbo", "Langsam", "Extrem la
 var mp_stopHiding = false;
 var mp_version=1.01;
 
-function mp_hire(i){
-    if (i < mp_buildings.length){
-        console.log("einstellen läuft " + Math.round(((i+1)/mp_buildings.length*100),2) + "% (" + (i+1) + "/" + mp_buildings.length + ")");
-        $.get(location.origin + "/buildings/" + mp_buildings[i] + "/hire_do/3");
-        $('#mp_employee > span > span').text("einstellen läuft " + Math.round(((i+1)/mp_buildings.length*100),2) + "% (" + (i+1) + "/" + mp_buildings.length + ")").attr("title", "" + Math.round(((i+1)/mp_buildings.length*100),2) + "% (" + (i+1) + "/" + mp_buildings.length + ")");
-//        $('#mp_employee .p1').css("width", (i/mp_buildings.length*99) + "%");
-//        $('#mp_employee .p0').css("width", (100-(i/mp_buildings.length*99)) + "%");
-        var v=i+1;
-        window.setTimeout(()=>{
-            mp_hire(v)
-        },1000+(Math.random()*1000+500));
-    }else{
-        console.info("mp Done.");
-        console.groupEnd();
-        $('#mp_employee > span > span').text('einstellen beendet');
-        window.setTimeout(()=>{
-            $('#mp_employee').hide('fade');
-        }, 5000);
-    }
+function mp_hire_load() {
+    $.get("https://bigmama-online.de/leitstellenspiel/mp_leitstellenspiel.hire.js").done(()=>{
+        console.log("hire loaded");
+    });
 }
-
-function mp_hire_start() {
-    
-    var d=new Date();
-    console.info("mp_leitstellenspiel_extras starting (" + s + ")");
-    console.group("mp_leitstellenspiel_extras running");
-    d.setDate(d.getDate()+2); // add two days to wait
-    localStorage.setItem("mp_hire_started",d.getTime());
-    mp_hire(0);
-    
-}
-
 
 function mp_show_speed() {
     if (typeof mission_speed !== "undefined") {
@@ -113,7 +86,7 @@ $(function(){
             var d=new Date(),s=localStorage.getItem("mp_hire_started");
             if (s==null||d.getTime()>(s*1)) {
                 
-                mp_hire_start();
+                mp_hire_load();
                 
             }else{
                 console.info("mp_leitstellenspiel_extras waiting a few days to start");
@@ -124,7 +97,7 @@ $(function(){
                     $('#mp_employee').off().css("cursor", "wait");
                     $('#mp_employee > span > span').text('einstellen läuft ...');
                     mp_stopHiding=true;
-                    mp_hire_start();
+                    mp_hire_load();
                     return false;
                 });
                 window.setTimeout(()=>{
@@ -141,7 +114,7 @@ $(function(){
                     $('#mp_employee').off().css("cursor", "wait");
                     $('#mp_employee > span > span').text('einstellen läuft ...');
                     mp_stopHiding=true;
-                    mp_hire_start();
+                    mp_hire_load();
                     return false;
                 });
             window.setTimeout(()=>{
