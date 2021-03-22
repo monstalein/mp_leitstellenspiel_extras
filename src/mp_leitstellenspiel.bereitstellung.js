0b324a549cc0b487d2a8a4fb40dbf7c8e8658c4f
Mag <monstamagp@gmx.de>
2021-03-22 16:41:33 +0100

    Anhänger jetzt extra auswählen

--- src/mp_leitstellenspiel.bereitstellung.js
+++ src/mp_leitstellenspiel.bereitstellung.js
@@ -16,7 +16,7 @@
 
         var tmp_builds = JSON.parse(mp_tmp);
 
-        var s = "<select id=\"mp_buildings\">";
+        var s = "<select id=\"mp_buildings\" class=\"btn\">";
         
         $(tmp_builds).each((i,e)=>{
             if (typeof buildings_there["building_" + e.building_id] !== "undefined") {
@@ -26,7 +26,11 @@
 
         s += "</select>";
         
-        $('#mp_loading_building').html("<label>Fahrzeuge von Wachen vor Ort: </label> " + s + " <button type=\"button\" id=\"mp_sel_dept\">Ausw&auml;hlen</button>");
+        $('#mp_loading_building').html("<label>Fahrzeuge von Wachen vor Ort: </label> " + s + 
+            " <button class=\"btn btn-secondary\" type=\"button\" id=\"mp_sel_dept\" title=\"Es k&ouml;nnen nacheinander auch mehrere Wachen ausgew&auml;hlt werden, und dann einmal aufgehoben werden\">Wache ausw&auml;hlen</button> " +
+            " oder <button class=\"btn btn-primary\" type=\"button\" id=\"mp_sel_trailer\" title=\"Zugfahrzeuge fahren automatisch mit - so bleiben keine Anh&auml;nger im Status 9 hier\">Alle Anh&auml;nger ausw&auml;hlen</button>" + 
+            " dann <button class=\"btn btn-warning\" type=\"submit\" name=\"submit\">Bereitstellung der Fahrzeuge aufheben</button>"
+        );
         
         $('#mp_sel_dept').click(()=>{
         
@@ -34,6 +38,22 @@
             
             $('input[building_id="' + id + '"]').prop("checked", "checked");//.each((i,e)=>{});
         
+        });
+        
+        $('#mp_sel_trailer').click(()=>{
+            
+            var vehi = JSON.parse(localStorage.aVehicleTypesNew);
+            
+            for (var i = 0; i < vehi.value.length; i++) {
+                
+                var t = vehi.value[i];
+                
+                if (typeof t.additional.trailer !== "undefined") {
+                    // ist ein Anh&auml;nger
+                    $('img[vehicle_type_id="' + t.id + '"]').parent().parent().find('input[class="vehicle_remove_checkbox"]').prop("checked", "checked");
+                }
+            }
+           
         });
 
     }
